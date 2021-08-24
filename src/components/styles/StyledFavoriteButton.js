6bd387207e3100d9838358c6bcea79f8309e91d1
Karcub <rokaszemumenyecske@gmail.com>
2021-08-24 20:50:23 +0200

    feat: added favorite button to cards

---
+++ src/components/styles/StyledFavoriteButton.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import filled from "../styles/star_filled.png";
+import empty from "../styles/star_empty.png";
+import hover from "../styles/star_hover.png";
+
+export const StyledFavoriteButton = styled.button`
+  top: 0;
+  left: 60px;
+  border: 0;
+  position: absolute;
+  background-color: inherit;
+  display: block;
+  cursor: pointer;
+  width: 33px;
+  height: 33px;
+  background-size: contain;
+  background-image: url(${empty});
+  background-repeat: no-repeat;
+
+  &:hover {
+    background-image: url("${hover}");
+  }
+`;
+
