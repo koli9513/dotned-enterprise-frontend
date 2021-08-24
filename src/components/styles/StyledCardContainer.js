cda6db0174ebc3d7d1cc49fe40110766616a6445
Karcub <rokaszemumenyecske@gmail.com>
2021-08-24 19:39:19 +0200

    feat: styled component for card container

---
+++ src/components/styles/StyledCardContainer.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledCardContainer = styled.div
+    .attrs({className: 'cards'})`
+  padding: 120px 50px;
+  border-radius: 5px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-column-gap: 20px;
+  grid-row-gap: 10px;
+  min-height: 537px;
+`;
