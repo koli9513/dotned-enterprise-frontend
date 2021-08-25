2676a1284adb1b3e0b8a7017df6150b7155cbcdc
Anita Nagy <anitanagy1989@gmail.com>
2021-08-25 13:22:58 +0200

    fix: remove page reload from deleteEvent

--- src/components/elements/EventCard.js
+++ src/components/elements/EventCard.js
@@ -1,43 +1,48 @@
-import {StyledSmallCard} from "../styles/StyledSmallCard";
-import {Link, useHistory} from "react-router-dom";
-import {StyledFavoriteButton} from "../styles/StyledFavoriteButton";
-import {createAPIEndpoint, ENDPOINTS} from "../../api";
+import { StyledSmallCard } from "../styles/StyledSmallCard";
+import { Link } from "react-router-dom";
+import { StyledFavoriteButton } from "../styles/StyledFavoriteButton";
+import { createAPIEndpoint, ENDPOINTS } from "../../api";
 
 const EventCard = (props) => {
-    const detailedViewUrl = `/event/${props.id}`;
-    let history = useHistory();
+  const detailedViewUrl = `/event/${props.id}`;
 
-    const deleteEvent = () => {
-        createAPIEndpoint(ENDPOINTS.EVENT)
-            .delete(JSON.stringify(props.id))
-            .then(() => {
-                console.log("Event deleted successfully");
-                // history.push("/events");
-                window.location.reload(false);
-            })
-            .catch((err) => console.log(err));
-    }
+  const deleteEvent = () => {
+    createAPIEndpoint(ENDPOINTS.EVENT)
+      .delete(JSON.stringify(props.id))
+      .then(() => {
+        props.setRequestData({});
+        console.log("Event deleted successfully");
+      })
+      .catch((err) => console.log(err));
+  };
 
-    return (
-        <StyledSmallCard>
-            <img src={props.image} alt="small card img"/>
-            <div className="date-info">
-                <span className="year">{props.year} </span>
-                <span className="month">{props.month}</span><br/>
-                <span className="day">{props.day}</span>
-            </div>
-            <div className="time-info">
-                <em className="hour">{props.hour}</em>
-                <em className="minute">{props.minute}</em>
-                <StyledFavoriteButton/>
-            </div>
-            <Link to={detailedViewUrl} className="name">{props.name}</Link>
+  return (
+    <StyledSmallCard>
+      <img src={props.image} alt="small card img" />
+      <div className="date-info">
+        <span className="year">{props.year} </span>
+        <span className="month">{props.month}</span>
+        <br />
+        <span className="day">{props.day}</span>
+      </div>
+      <div className="time-info">
+        <em className="hour">{props.hour}</em>
+        <em className="minute">{props.minute}</em>
+        <StyledFavoriteButton />
+      </div>
+      <Link to={detailedViewUrl} className="name">
+        {props.name}
+      </Link>
 
-            <button className="delete" onClick={deleteEvent}>🗑</button>
+      <button className="delete" onClick={deleteEvent}>
+        🗑
+      </button>
 
-            <div className="tags">{props.category}, <span>{props.city}</span></div>
-        </StyledSmallCard>
-    );
+      <div className="tags">
+        {props.category}, <span>{props.city}</span>
+      </div>
+    </StyledSmallCard>
+  );
 };
 
 export default EventCard;
--- src/components/elements/EventList.js
+++ src/components/elements/EventList.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import { createAPIEndpoint, ENDPOINTS } from "../../api";
 import EventCard from "./EventCard";
-import {StyledCardContainer} from "../styles/StyledCardContainer";
+import { StyledCardContainer } from "../styles/StyledCardContainer";
 
 const EventList = () => {
   const [events, setEvents] = useState([]);
+  const [requestData, setRequestData] = useState({});
 
   useEffect(() => {
     createAPIEndpoint(ENDPOINTS.EVENT)
@@ -13,24 +14,25 @@ const EventList = () => {
         setEvents(res.data);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [requestData]);
 
   return (
     <StyledCardContainer>
       {events.map((event, index) => (
-          <EventCard
-              key={index}
-              image={event.image}
-              city={event.city}
-              name={event.name}
-              year={event.formattedDate[0]}
-              month={event.formattedDate[1]}
-              day={event.formattedDate[2]}
-              hour={event.formattedDate[3]}
-              minute={event.formattedDate[4]}
-              id={event.id}
-              category={event.category}
-          />
+        <EventCard
+          key={index}
+          image={event.image}
+          city={event.city}
+          name={event.name}
+          year={event.formattedDate[0]}
+          month={event.formattedDate[1]}
+          day={event.formattedDate[2]}
+          hour={event.formattedDate[3]}
+          minute={event.formattedDate[4]}
+          id={event.id}
+          category={event.category}
+          setRequestData={setRequestData}
+        />
       ))}
     </StyledCardContainer>
   );
