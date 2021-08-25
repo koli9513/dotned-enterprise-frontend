a2c05a06d25a69193acbd19245ae280aea0aa7ad
Karcub <rokaszemumenyecske@gmail.com>
2021-08-25 09:41:36 +0200

    feat: added navbar styled component

---
+++ src/components/styles/StyledNavbarLink.js
@@ -0,0 +1,31 @@
+import styled, {css} from "styled-components";
+import {Link} from "react-router-dom";
+
+export const StyledNavbarLink = styled(Link)`
+  position: relative;
+  left: 33%;
+  font-family: 'PT Serif',Georgia,serif;
+  background: #fffdfd;
+  color: #bc3926;
+  float: left;
+  height: 34px;
+  line-height: 34px;
+  text-align: center;
+  font-size: 1.1em;
+  margin-top: 6px;
+  padding: 0 25px;
+  text-transform: uppercase;
+  text-decoration: none;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+
+  ${props => props && css`
+    transition: 0.1s;
+
+    &:hover {
+      background: #bc3926;
+      color: #fff;
+    }
+  `}
+`;
---
+++ src/components/styles/StyledSearchButton.js
@@ -0,0 +1,19 @@
+import styled, {css} from "styled-components";
+import {Link} from "react-router-dom";
+
+export const StyledSearchButton = styled.button`
+  width: 40px;
+  height: 40px;
+  float: right;
+  background: #bc3926;
+  border: none;
+  border-radius: 50%;
+  color: #fff;
+  font-size: 1.26666667em;
+  line-height: 35px;
+  margin-right: 0;
+  text-align: center;
+  top: 0;
+  right: 2%;
+  position: absolute;
+`;
