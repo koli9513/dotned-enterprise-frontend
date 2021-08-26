af1b48421318c510e6e5138ba0578d0fa0edd963
Karcub <rokaszemumenyecske@gmail.com>
2021-08-26 07:37:19 +0200

    feat: navbar always stays on top and foremost

--- src/components/styles/StyledNavbar.js
+++ src/components/styles/StyledNavbar.js
@@ -5,6 +5,8 @@ export const StyledNavbar = styled.div`
   background: #f1f1f1;
   border-bottom: 5px solid #bc3926;
   width: 100%;
-  //float: left;
-  //text-align: center;
+  overflow: hidden;
+  position: fixed;
+  z-index: 100;
+  top: 0;
 `;
